8646f7d7bf0691729487bc1b60f145fa7a80b3b8
gzhen <1544445921@qq.com>
2022-04-02 15:56:50 +0800

    提交aop

--- aop/src/main/java/com/demo/AopServerStarter.java
+++ aop/src/main/java/com/demo/AopServerStarter.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-//exposeProxy=true 可以在方法内部调用再进行增强 proxyTargetClass=true必须使用cglib代理
+//    exposeProxy=true 可以在方法内部调用再进行增强 proxyTargetClass=true必须使用cglib代理
 @EnableAspectJAutoProxy(exposeProxy = true)
 public class AopServerStarter {
     public static void main(String[] args) {
